116101fed81f1c2a11e3803b01538692d726a272
Dylan Marini <dylan.marini@g.austincc.edu>
2018-08-30 22:19:35 -0500

    cloned git and added my udpated code

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            2,
+            5,
+            3,
+            1,
+            2,
+            5,
+            1,
+            2,
+            3,
+            5
+          ]
+    },
+    {
+        name:"Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            1,
+            1,
+            3,
+            2,
+            5,
+            3,
+            1,
+            4,
+            4,
+            3
+          ]
+    },
+
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friends;

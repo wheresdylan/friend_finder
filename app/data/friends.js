96e5d293ceb677ee97a8ad3549ce77061f079610
Dylan Marini <dylan.marini@g.austincc.edu>
2018-08-31 17:19:40 -0500

    progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,7 +16,7 @@ var friends = [
           ]
     },
     {
-        name:"Ahmed",
+        name:"Rick",
         photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores:[
             2,
@@ -32,7 +32,7 @@ var friends = [
           ]
     },
     {
-        name:"Ahmed",
+        name:"Morty",
         photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         scores:[
             1,
